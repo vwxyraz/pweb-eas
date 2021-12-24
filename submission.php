b681114ee493c469d16b222348ea6333b001ee9f
Fayha Syifa Qalbi <72442637+sfayhaa@users.noreply.github.com>
2021-12-24 21:52:43 +0700

    Add files via upload

---
+++ submission.php
@@ -0,0 +1,121 @@
+
+</html>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Submission</title>
+
+    <link href="style.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="sidenav">
+        <div class="profile">
+            <img src="profile.jpg">
+            <div class="column">
+                <h2 class="profile-name"> Vyra Fania Adelina</h2>
+                <p>Siswa</p>
+            </div>
+        </div>
+        <a href="#">Timetable View</a>
+        <a href="#">Result View</a>
+        <a href="#">Assignment Submission</a>
+        <a href="#">Study Material</a>
+    </div>
+
+    <div class="content">
+        <header>
+            <h1>Assignment Submission</h1>
+            <h3>Submission / </h3>
+        </header>
+
+        <div class="row">
+
+            <div class="box">
+                <div class="card-col">
+                    <div class="card-row">
+                        <div class="card-title">
+                            <p>Tugas 1</p>
+                        </div>
+                        <h2 class="card-title-side">Matematika</h2>
+                    </div>
+
+                    <div class="card-row">
+                        <div class="card-info">
+                            <h4>Tugas Buku Paket Hal. 134</h4>
+                            <center>
+                                <form action="upload.php" method="post" enctype="multipart/form-data">
+                                <input type="file" name="fileToUpload" id="fileToUpload">
+                                <input type="submit" value="Upload Image" name="submit">
+                                </form>
+                            </center>
+                        </div>
+                    </div>
+                   
+                </div>
+            </div>
+        </div>
+        <div class="row">
+
+<div class="box">
+    <div class="card-col">
+        <div class="card-row">
+            <div class="card-title">
+                <p>Tugas 2</p>
+            </div>
+            <h2 class="card-title-side">Fisika</h2>
+        </div>
+
+        <div class="card-row">
+            <div class="card-info">
+                <h4>Tugas Essay Pembuktian Hukum Newton 1</h4>
+                <center>
+                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload Image" name="submit">
+                    </form>
+                </center>
+            </div>
+        </div>
+       
+    </div>
+</div>
+        </div>
+<div class="row">
+
+<div class="box">
+    <div class="card-col">
+        <div class="card-row">
+            <div class="card-title">
+                <p>Tugas 3</p>
+            </div>
+            <h2 class="card-title-side">Biologi</h2>
+        </div>
+
+        <div class="card-row">
+            <div class="card-info">
+                <h4>Tugas Buku Paket Hal. 17</h4>
+                <center>
+                    <form action="upload.php" method="post" enctype="multipart/form-data">
+                    <input type="file" name="fileToUpload" id="fileToUpload">
+                    <input type="submit" value="Upload Image" name="submit">
+                    </form>
+                </center>
+            </div>
+        </div>
+       
+    </div>
+</div>
+</div>
+
+    </div>
+
+</body>
+
+</html>
---
+++ upload.php
@@ -0,0 +1,50 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+/*if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "<script type='text/javascript'>alert('File is an image - ')</script>" . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "<script type='text/javascript'>alert('File is not an image.')</script>";
+    $uploadOk = 0;
+  }
+}
+*/
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "<script type='text/javascript'>alert('Sorry, file already exists.')</script>";
+  $uploadOk = 0;
+}
+
+// Check file size
+else if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "<script type='text/javascript'>alert('Sorry, your file is too large.')</script>";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+else if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "<script type='text/javascript'>alert('Sorry, only JPG, JPEG, PNG & GIF files are allowed.')</script>";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+else if ($uploadOk == 0) {
+  echo "<script type='text/javascript'>alert('Sorry, your file was not uploaded.')</script>";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "<script type='text/javascript'>alert('The file ')</script>". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "<script type='text/javascript'>alert('failed!')</script>";
+  }
+}
+?>
