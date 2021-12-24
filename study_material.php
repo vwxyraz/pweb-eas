527c3415284537437914c694bc2c14cfcda9810e
Fidhia Ainun Khofifah <90237196+fidhiaakaa@users.noreply.github.com>
2021-12-24 19:35:55 +0700

    Add files via upload

--- study_material.php
+++ study_material.php
@@ -4,7 +4,7 @@
 <head>
     <title>Study Material</title>
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style-f.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
